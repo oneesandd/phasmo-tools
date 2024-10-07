4deab0ba57bc5d14b3748c3885bd228bdc8d3724
oneesandd <179961906+oneesandd@users.noreply.github.com>
2024-10-07 20:29:53 -0300

    Update +page.svelte

--- src/routes/objectives/+page.svelte
+++ src/routes/objectives/+page.svelte
@@ -135,7 +135,7 @@
 
     async function runOCR(canvasFull) {
         await processFrame(canvasFull);
-        await new Promise(resolve => setTimeout(resolve, 100)); // Throttle processing, adjust delay as needed
+        await new Promise(resolve => setTimeout(resolve, 1000)); // Throttle processing, adjust delay as needed
     }
 
     async function logTesseract() {
